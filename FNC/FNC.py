2f73d007af2d5ab7c040fc849d1420fb255729ca
Cuervinho <50245754+Cuervinho@users.noreply.github.com>
2020-04-23 11:22:29 -0500

    Update FNC.py

--- FNC/FNC.py
+++ FNC/FNC.py
@@ -49,24 +49,88 @@ def enFNC(A):
 def Tseitin(A, letrasProposicionalesA):
     letrasProposicionalesB = [chr(x) for x in range(256, 1200)]
     assert(not bool(set(letrasProposicionalesA) & set(letrasProposicionalesB))), u"¡Hay letras proposicionales en común!"
-
-    #  IMPLEMENTAR AQUI ALGORITMO TSEITIN
+    atomos = letrasProposicionalesA + letrasProposicionalesB
+    L = []
+    pila = []
+    i = -1
+    s = A[0]
     
-    pass
+    while len(A) > 0:
+        if s in atomos and len(pila) > 0 and pila[-1] == "-":
+            i += 1
+            atomo = letrasProposicionalesB[i]
+            pila = pila[:-1]
+            pila.append(atomo)
+            L.append(atomo + "=" + "-" + s)
+            A = A[1:]
+            if len(A) > 0:
+                s = A[0]
+        elif s == ")":
+            w = pila[-1]
+            O = pila[-2]
+            v = pila[-3]
+            pila = pila[:len(pila) - 4]
+            i += 1
+            atomo = letrasProposicionalesB[i]
+            L.append(atomo + "=" + "(" + v + O + w + ")")
+            s = atomo
+        else:
+            pila.append(s)
+            A = A[1:]
+            if len(A) > 0:
+                s = A[0]
+    
+    B = ""
+    if i < 0:
+        atomo = pila[-1]
+    else:
+        atomo = letrasProposicionalesB[i]
+    
+    for X in L:
+        Y = enFNC(X)
+        B += "Y" + Y
+    
+    B = atomo + B
+    return B
+    #pass
 
 # Subrutina Clausula para obtener lista de literales
 # Input: C (cadena) una clausula
 # Output: L (lista), lista de literales
 # Se asume que cada literal es un solo caracter
 def Clausula(C):
-
-    #  IMPLEMENTAR AQUI ALGORITMO CLAUSULA
-    pass
+    L = []
+    while len(C) > 0:
+        s = C[0]
+        if s == "O":
+            C = C[1:]
+        elif s == "-":
+            literal = s + C[1]
+            L.append(literal)
+            C = C[2:]
+        else:
+            L.append(s)
+            C = C[1:]
+    return L
+    
+#    pass
 
 # Algoritmo para obtencion de forma clausal
 # Input: A (cadena) en notacion inorder en FNC
 # Output: L (lista), lista de listas de literales
 def formaClausal(A):
-
-    #  IMPLEMENTAR AQUI ALGORITMO FORMA CLAUSAL
-    pass
+    L = []
+    i = 0
+    while len(A) > 0:
+        if i >= len(A):
+            L.append(Clausula(A))
+            A = []
+        else:
+            if A[i] == "Y":
+                L.append(Clausula(A[:i]))
+                A = A[i + 1:]
+                i = 0
+            else:
+                i += 1
+    return L
+    #pass
