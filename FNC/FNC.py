12da9b0bb18941ce092d9e0d0c39657724d3fe66
sebastianssg <56741758+sebastianssg@users.noreply.github.com>
2020-04-23 11:20:10 -0500

    Add files via upload

---
+++ FNC/FNC.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+
+# Subrutinas para la transformacion de una
+# formula a su forma clausal
+
+# Subrutina de Tseitin para encontrar la FNC de
+# la formula en la pila
+# Input: A (cadena) de la forma
+#                   p=-q
+#                   p=(qYr)
+#                   p=(qOr)
+#                   p=(q>r)
+# Output: B (cadena), equivalente en FNC
+def enFNC(A):
+    assert(len(A)==4 or len(A)==7), u"Fórmula incorrecta!"
+    B = ''
+    p = A[0]
+    # print('p', p)
+    if "-" in A:
+        q = A[-1]
+        # print('q', q)
+        B = "-"+p+"O-"+q+"Y"+p+"O"+q
+    elif "Y" in A:
+        q = A[3]
+        # print('q', q)
+        r = A[5]
+        # print('r', r)
+        B = q+"O-"+p+"Y"+r+"O-"+p+"Y-"+q+"O-"+r+"O"+p
+    elif "O" in A:
+        q = A[3]
+        # print('q', q)
+        r = A[5]
+        # print('r', r)
+        B = "-"+q+"O"+p+"Y-"+r+"O"+p+"Y"+q+"O"+r+"O-"+p
+    elif ">" in A:
+        q = A[3]
+        # print('q', q)
+        r = A[5]
+        # print('r', r)
+        B = q+"O"+p+"Y-"+r+"O"+p+"Y-"+q+"O"+r+"O-"+p
+    else:
+        print(u'Error enENC(): Fórmula incorrecta!')
+
+    return B
+
+# Algoritmo de transformacion de Tseitin
+# Input: A (cadena) en notacion inorder
+# Output: B (cadena), Tseitin
+def Tseitin(A, letrasProposicionalesA):
+    letrasProposicionalesB = [chr(x) for x in range(256, 1200)]
+    assert(not bool(set(letrasProposicionalesA) & set(letrasProposicionalesB))), u"¡Hay letras proposicionales en común!"
+
+    #  IMPLEMENTAR AQUI ALGORITMO TSEITIN
+    
+    letrasProposicionalesA
+    letrasProposicionalesB = [chr(x) for x in range(256, 1200)]
+    L = [] # Inicializamos lista de conjunciones
+    Pila = [] # Inicializamos pila
+    i= -1 # Inicializamos contador de variables nuevas
+    s = A[0] # Inicializamos simbolo de trabajo
+
+
+    while(len(A) > 0):
+        if (s == atomo and Pila!=[] and Pila[-1] == '¬'):
+            i += 1
+            atomo = letrasProposicionalesB[i]
+            Pila = Pila[:-1]
+            Pila.append(atomo)
+            L.append(atomo↔¬s)
+            A = A[1:]
+            if(len(A) > 0):
+                s = A[0]
+        elif (s == ')'):
+            w = Pila[-1]
+            u = Pila[-2]
+            v = Pila[-3]
+            Pila = Pila[:len(Pila)-4]
+            i += 1
+            atomo = letrasProposicionalesB[i]
+            L.append((atomo ↔(vuw))
+            s = atomo
+        else:
+            Pila.append(s)
+            A = A[1:]
+            if(len(A)>0):
+                s = A[0]
+                B = ''
+            if(i < 0):
+                atomo = Pila[-1]
+            else:
+                atomo = letrasProposicionalesB[i]
+            for X in L:
+                #Y = X en su respectiva FNC
+                B += Y
+                B = atomo + B
+    return B
+    
+    pass
+
+# Subrutina Clausula para obtener lista de literales
+# Input: C (cadena) una clausula
+# Output: L (lista), lista de literales
+# Se asume que cada literal es un solo caracter
+def Clausula(C):
+
+    #  IMPLEMENTAR AQUI ALGORITMO CLAUSULA
+    pass
+
+# Algoritmo para obtencion de forma clausal
+# Input: A (cadena) en notacion inorder en FNC
+# Output: L (lista), lista de listas de literales
+def formaClausal(A):
+
+    #  IMPLEMENTAR AQUI ALGORITMO FORMA CLAUSAL
+    pass
